2ceb9510a14f33912b6f65d7e497cca4cfcfb099
hastimoeini <118931264+hastimoeini@users.noreply.github.com>
2026-07-05 17:49:07 +0430

    add complete docker configuration

--- backend/appointments/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import permissions
-
-class IsDoctor(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_authenticated and hasattr(request.user, 'doctorprofile')
-
-class IsPatient(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_authenticated and hasattr(request.user, 'patientprofile')
